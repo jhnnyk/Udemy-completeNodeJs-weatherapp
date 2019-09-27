699054539baf11181c7c8b627e13cb1f1de53790
John Knotts <jhnnyk@gmail.com>
2019-09-27 16:24:20 -0600

    handling errors

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const request = require('request');
 
+// Weather
 const url =
   'https://api.darksky.net/forecast/a39b2fd254a7eab701964331d2d240c9/37.8267,-122.4233';
 
 request({ url: url, json: true }, (error, response) => {
-  // console.log(response.body.currently);
-  console.log(
-    response.body.daily.data[0].summary +
-      ' It is currently ' +
-      response.body.currently.temperature +
-      ' degrees out. There is a ' +
-      response.body.currently.precipProbability +
-      '% chance of rain.'
-  );
+  if (error) {
+    console.log('Unable to connect to weather service!');
+  } else if (response.body.error) {
+    console.log('Unable to find location');
+  } else {
+    console.log(
+      response.body.daily.data[0].summary +
+        ' It is currently ' +
+        response.body.currently.temperature +
+        ' degrees out. There is a ' +
+        response.body.currently.precipProbability +
+        '% chance of rain.'
+    );
+  }
 });
 
 // Geocoding
@@ -20,7 +26,13 @@ const geocodeURL =
   'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamhubnlrIiwiYSI6ImNrMTJteTZsZjAwZHozZW4wMnVoNGpnaWkifQ.JRcpHE6B8o7bt7HMxxEtvw';
 
 request({ url: geocodeURL, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0];
-  console.log(latitude, longitude);
+  if (error) {
+    console.log('Unable to connect to location services!');
+  } else if (response.body.features.length === 0) {
+    console.log('Unable to find that location. Try another search.');
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude);
+  }
 });
