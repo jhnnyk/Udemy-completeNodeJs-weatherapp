eb84142def1f5ca114c858260e5eb1007f9c8c89
John Knotts <jhnnyk@gmail.com>
2019-09-27 15:56:52 -0600

    geocode HTTP request challenge

--- app.js
+++ app.js
@@ -14,3 +14,13 @@ request({ url: url, json: true }, (error, response) => {
       '% chance of rain.'
   );
 });
+
+// Geocoding
+const geocodeURL =
+  'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamhubnlrIiwiYSI6ImNrMTJteTZsZjAwZHozZW4wMnVoNGpnaWkifQ.JRcpHE6B8o7bt7HMxxEtvw';
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[1];
+  const longitude = response.body.features[0].center[0];
+  console.log(latitude, longitude);
+});
